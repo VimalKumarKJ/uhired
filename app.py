a4867f91a4d3c6bdd3e9d27802509bcbcd139ad8
Vimal Kumar .K.J <76762337+kickmyd2602@users.noreply.github.com>
2024-12-31 18:48:48 +0530

    Connect the database to the client and dynamically fetch the data from the db and display it on the client end

--- app.py
+++ app.py
@@ -1,53 +1,32 @@
 from flask import Flask, render_template, jsonify
+from sqlalchemy import text
 from datetime import date
+from database import engine
 app = Flask(__name__)
-
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Software Developer - I',
-    'company': 'ABC Company',
-    'location': 'Bangalore, India',
-    'experience': '1',
-    'Salary': '6.5LPA'
-  },
-  {
-    'id': 2,
-    'title': 'Frontend Developer',
-    'company': 'Xyz corp.',
-    'location': 'Chennai, India',
-    'experience': '3',
-    'Salary': '10LPA'
-  },
-  {
-    'id': 3,
-    'title': 'Backend Developer',
-    'company': 'Kekron-Mekron corp.',
-    'location': 'Hydrabad, India',
-    'experience': '2',
-    'Salary': '20LPA'
-  },
-  {
-    'id': 4,
-    'title': 'Data Analyst',
-    'company': 'Rotten Tomatoes',
-    'location': 'Bangalore, India',
-    'experience': '7',
-    'Salary': '50LPA'
-  }
-  
-]
 
 current_date = date.today()
 current_year = current_date.year
 
+def get_jobData_from_db():
+  with engine.connect() as connect:
+    result = connect.execute(text("SELECT * FROM jobs"))
+    jobs= []
+    for row in result.all():
+        jobs.append(dict(row._mapping))
+    return jobs
+  
+
+
+
 @app.route("/")
-def hello_world():
-  return render_template('home.html', jobs_list = JOBS, year = current_year)
+def home():
+  jobs = get_jobData_from_db()
+  return render_template('home.html', jobs_list = jobs, year = current_year)
 
 @app.route("/api/jobs")
 def api():
-  return jsonify(JOBS)
+  jobs = get_jobData_from_db()
+  return jsonify(jobs)
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', port = 8080, debug = True)
--- database.py
+++ database.py
@@ -13,9 +13,3 @@ DBNAME = os.getenv("dbname")
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
-
-try:
-    with engine.connect() as connection:
-        print("Connection successful!")
-except Exception as e:
-    print(f"Failed to connect: {e}")
